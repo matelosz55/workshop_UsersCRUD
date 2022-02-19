930edbf7d3e4192fd6e5ebc0e620f9c92295f35b
matelosz55 <mat.chodak@gmail.com>
2022-02-19 13:54:16 +0100

    ...

--- src/main/java/pl/coderslab/users/UserAdd.java
+++ src/main/java/pl/coderslab/users/UserAdd.java
@@ -1,5 +1,8 @@
 package pl.coderslab.users;
 
+import pl.coderslab.dao.UserDao;
+import pl.coderslab.entity.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,10 +14,27 @@ import java.io.IOException;
 @WebServlet("/user/add")
 public class UserAdd extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        getServletContext().getRequestDispatcher("/users/list.jsp")
+        getServletContext().getRequestDispatcher("/users/add.jsp")
                 .forward(request, response);
 
+
     }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String userName = request.getParameter("userName");
+        String email = request.getParameter("userEmail");
+        String password = request.getParameter("userPassword");
+        User user = new User();
+        user.setUserName(userName);
+        user.setEmail(email);
+        user.setPassword(password);
+        UserDao userDao = new UserDao();
+        userDao.create(user);
+        response.sendRedirect("/user/list");
+
+
+    }
+
 
 }
 
--- src/main/java/pl/coderslab/users/UserEdit.java
+++ src/main/java/pl/coderslab/users/UserEdit.java
@@ -1,5 +1,8 @@
 package pl.coderslab.users;
 
+import pl.coderslab.dao.UserDao;
+import pl.coderslab.entity.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,14 +10,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
 @WebServlet("/user/edit")
 public class UserEdit extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        getServletContext().getRequestDispatcher("/users/list.jsp")
-                .forward(request, response);
-
+        getServletContext().getRequestDispatcher("/users/edit.jsp").forward(request, response);
     }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        User user = new User();
+        String id = request.getParameter("id");
 
+        user.setId(Long.parseLong(id));
+        user.setUserName(request.getParameter("userName"));
+        user.setEmail(request.getParameter("userEmail"));
+        user.setPassword(request.getParameter("userPassword"));
+
+        UserDao userDao = new UserDao();
+        userDao.update(user);
+
+        response.sendRedirect("/user/list");
+    }
 }
-
